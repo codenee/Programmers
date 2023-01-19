e01dac2be73f34c87205125971dd9d74cedd32a9
codenee <tanedoran@gmail.com>
2023-01-19 13:12:05 +0900

    131128

--- Level_1/131128-Number-Partner/131128-Number-Partner2.cpp
+++ Level_1/131128-Number-Partner/131128-Number-Partner2.cpp
@@ -1,37 +1,37 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 string solution(string X, string Y) {
     string answer = "";
-    unordered_map<char, int> mx;
-    unordered_map<char, int> my;
-
+    unordered_map<char, int> mx, my;
+    
+    /*
     for(char i = '0'; i <='9'; i++){
         mx.insert(make_pair(i, 0));
         my.insert(make_pair(i, 0));
     }
-
-    for(auto c : X){
-            mx[c]++;
+    */
+    
+    for(const auto& it : X){
+        mx[it] += 1;
     }
-
-    for(auto c : Y){
-            my[c]++;
+    for(const auto& it : Y){
+        my[it] += 1;
     }
-
-    for (char i = '9'; i >= '0'; i--){
-        int m = min(mx[i], my[i]);
+    for(char c = '9'; c >= '0'; c--){
+        int m = min(mx[c], my[c]);
         for(int j = 0; j < m; j++){
-            answer += i;
+            answer += c;
         }
     }
-
-    if (answer == "") return "-1";
-    if (answer[0] == '0') return "0";
-
-    return answer;
-}
-int main(){
-
-    string x{"100"}, y{"123450"};
-    string reuslt = solution(x, y);
+    if(answer == ""){
+        return "-1";
+    }
+    if(answer[0] == '0'){
+        return "0";
+    }
     
-    return 0;
+    return answer;
 }
