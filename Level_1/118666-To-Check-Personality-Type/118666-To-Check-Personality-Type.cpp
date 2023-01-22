baa55684f8a85c6db2a72a6d66212a1d9e9aed4d
codenee <tanedoran@gmail.com>
2023-01-22 19:39:20 +0900

    118666

---
+++ Level_1/118666-To-Check-Personality-Type/118666-To-Check-Personality-Type.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> survey, vector<int> choices) {
+  string answer = "";
+
+	map<char, int> mk;
+	for (int i = 0; i < choices.size(); i++) {
+		for (int j = 0; j < survey.size(); j++) {
+			int cnt = 0;
+			if (choices[i] <= 4) {
+				cnt = 4 - choices[i];
+				mk[survey[i][0]] += cnt;
+				break;
+
+			}
+			else {
+				cnt = choices[i] - 4;
+				mk[survey[i][1]] += cnt;
+				break;
+			}
+		}
+		
+	}
+
+	vector<char> vec = { 'R' , 'T' , 'C' , 'F' , 'J' , 'M' , 'A', 'N' };
+
+	int pre = 0;
+	for (int i = 1; i < vec.size(); i += 2) {
+		
+		char c;
+		if (mk[vec[pre]] == mk[vec[i]]) {
+			c = vec[i] < vec[pre] ? vec[i] : vec[pre];
+		}
+		else {
+			c= mk[vec[pre]] < mk[vec[i]] ? vec[i] : vec[pre];
+		}
+		pre += 2;
+		answer += c;
+	}
+
+	return answer;
+}
---
+++ Level_1/118666-To-Check-Personality-Type/README.md
@@ -0,0 +1,6 @@
+<h2><a href="https://school.programmers.co.kr/learn/courses/30/lessons/118666">118666. 성격 유형 검사하기</a></h2><h3>Level 1</h3>
+A one-dimensional array of strings with metrics to judge for each question and a one-dimensional integer array with choices selected by the tester are given as parameters. At this time, complete the solution function to return the test results of the personality type of the inspector in the order of the index number.
+<p>&nbsp;</p>
+<p><strong class="example">Example 1:</strong></p>
+<pre><strong>Input:</strong> survey=["AN", "CF", "MJ", "RT", "NA"], choices = [5, 3, 2, 7, 5]
+<strong>Output:</strong> "TCMA" </pre>
